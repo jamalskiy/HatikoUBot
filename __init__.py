6131504c9097bb01909a751823acc5bfb9cbd600
Jamalskiy <155892199+jamalskiy@users.noreply.github.com>
2024-03-10 04:17:48 +0300

    ✨ HatikoUserBot ✨

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from .scan import handle_scan_command
-from .helpcommand import handle_help_command
-from .animationcommand import handle_animation_command
-from .mooncommand import handle_moon_command
-from .minimulti import handle_minimulti_command
-from .spamcommand import handle_spam_command
-from .goulcommand import handle_goul_command
