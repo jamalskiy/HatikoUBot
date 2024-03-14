0a605682d21b748612e9b717e4f8f325b50e1719
PashaSalt <162937401+PashaSalt@users.noreply.github.com>
2024-03-15 02:19:10 +0300

    ✨ HatikoUserBot ✨

--- HatikoUserBot.py
+++ HatikoUserBot.py
@@ -34,29 +34,35 @@ client = TelegramClient('random', api_id, api_hash)
 
 @client.on(events.NewMessage(pattern=re.compile(r'^\!(еж|ёж|ежик|ёжик)', re.IGNORECASE)))
 async def minimulti_handler(event: events.NewMessage.Event):
-    await handle_minimulti_command(event, user_id)
+    if event.sender_id == int(user_id):
+        await handle_minimulti_command(event)
 
 @client.on(events.NewMessage(pattern=re.compile(r'^\!(scan|скан)', re.IGNORECASE)))
 async def scan_handler(event: events.NewMessage.Event):
-    msg = await event.message.get_reply_message()
-    reply_text = await handle_scan_command(event, msg, user_id)
-    await event.edit(reply_text)
+    if event.sender_id == int(user_id):
+        msg = await event.message.get_reply_message()
+        reply_text = await handle_scan_command(event, msg)
+       await event.edit(reply_text)
 
 @client.on(events.NewMessage(pattern=re.compile(r'^\!(spam|спам)', re.IGNORECASE)))
 async def spam_handler(event: events.NewMessage.Event):
-    await handle_spam_command(event, user_id)
+    if event.sender_id == int(user_id):
+        await handle_spam_command(event)
 
 @client.on(events.NewMessage(pattern=r'^\!ты гуль\?', outgoing=True))
 async def goul_handler(event: events.NewMessage.Event):
-    await handle_goul_command(event, user_id)
+    if event.sender_id == int(user_id):
+        await handle_goul_command(event)
 
 @client.on(events.NewMessage(pattern=re.compile(r'^\!(moon|луна)', re.IGNORECASE)))
 async def moon_command_handler(event: events.NewMessage.Event):
-    await handle_moon_command(event, user_id)
+    if event.sender_id == int(user_id):
+        await handle_moon_command(event)
 
 @client.on(events.NewMessage(pattern=re.compile(r'^(❤️ magick|❤️ магия)', re.IGNORECASE)))
 async def animation_command_handler(event: events.NewMessage.Event):
-    await handle_animation_command(event, user_id)
+    if event.sender_id == int(user_id):
+        await handle_animation_command(event)
 
 @client.on(events.NewMessage(pattern=re.compile(r'^\!(команды|хелп)', re.IGNORECASE)))
 async def helpcommand(event: events.NewMessage.Event):
